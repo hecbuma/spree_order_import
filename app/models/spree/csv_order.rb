f2cf1f755f843b130d377b0bcfb60db2cd4c97f6
Hector Bustillos <hector.bustillos@crowdint.com>
2014-02-06 22:26:19 -0600

    move order creation process to a transition to be able to to rollback everything on failure, also I added a couple of extra validations

--- app/models/spree/csv_order.rb
+++ app/models/spree/csv_order.rb
@@ -31,121 +31,135 @@ class Spree::CsvOrder < ActiveRecord::Base
     self.process!
     open_file = open_file2 = File.open file.path
     orders = []
-    begin
-      ::CSV.foreach(open_file,{:headers => true}) do |row|
+    errors = {}
+    self.transaction do
+      begin
+        ::CSV.foreach(open_file,{:headers => true}) do |row|
 
-        next if Spree::Order.where(:number => row["Order Number"]).first
-        #order info
-        order = Spree::Order.new number: row["Order Number"]
-        order.customer_group = row["B2B/B2C"] unless row["B2B/B2C"].blank?
-        order.po_number = row["Customer PO Number"]
-        order.carrier_account_number = row["Customer UPS"]
-        order.sop_code = row["SOP CODE"]
-        order.gift_wrap = true unless row["Gift Wrap"].blank?
-        5.times do |i|
-          special_intruction_x = "Special Instruction #{i+1}"
-          order.special_instructions << row[special_intruction_x]
-        end
-        order.save
-
-        user = Spree.user_class.where(:email => row['Email'])
-        # user_group_id = Spree::UserGroup.find_by_name(row["Customer type"]).id if row["Customer type"] != "DRTC:LBT"
-        bill_country_id = Spree::Country.find_by_name(row["Billing Country"]).id
-        bill_state_id = Spree::State.find_by_name(row["Billing State"]).id
-        ship_country_id = Spree::Country.find_by_name(row["Shipping Country"]).id
-        ship_state_id = Spree::State.find_by_name(row["Shipping State"]).id
-        user_details = {"email"=> row["Email"], "bill_address_attributes"=>{"firstname"=> row["Order First Name"],
-                                                                           "lastname"=> row["Order Last Name"],
-                                                                           "company"=> row["Order Company Name"],
-                                                                           "address1"=> row["Billing Address1"],
-                                                                           "address2"=> row["Billing Address2"],
-                                                                           "city"=> row["Billing City"], "zipcode"=>row["Billing ZIP"],
-                                                                           "country_id"=> bill_country_id, "state_id"=> bill_state_id,
-                                                                           "phone"=> row["Billing Phone"]},
-                                                "ship_address_attributes"=>{"firstname"=> row["Order First Name"],
-                                                                           "lastname"=> row["Order Last Name"],
-                                                                           "company"=> row["Order Company Name"],
-                                                                           "address1"=> row["Shipping Address1"],
-                                                                           "address2"=> row["Shipping Address2"],
-                                                                           "city"=> row["Shipping City"], "zipcode"=>row["Shipping ZIP"],
-                                                                           "country_id"=> ship_country_id, "state_id"=> ship_state_id,
-                                                                           "phone"=> row["Shipping Phone"]},
-                                                "customer_type" => row["Customer type"] }
-
-        #Create Shippment
-        stock_location_id = Spree::StockLocation.find_by_name(row["Fulfilled From"]).id
-        shipment = order.shipments.create(:number => row['Shipment Number'], :stock_location_id => stock_location_id)
-
-        #Assing Variants
-        ::CSV.foreach(open_file2,{:headers => true}) do |row_clon|
-          if row_clon["Order Number"] == row["Order Number"]
-            variant = Spree::Variant.find_by_sku(row_clon["Sku"])
-            quantity = row_clon["Number of Items"].to_i
-            order.contents.add(variant, quantity, nil, shipment)
+          next if Spree::Order.where(:number => row["Order Number"]).first
+          #order info
+          order = Spree::Order.new number: row["Order Number"]
+          order.customer_group = row["B2B/B2C"] unless row["B2B/B2C"].blank?
+          order.po_number = row["Customer PO Number"]
+          order.carrier_account_number = row["Customer UPS"]
+          order.sop_code = row["SOP CODE"]
+          order.gift_wrap = true unless row["Gift Wrap"].blank?
+          5.times do |i|
+            special_intruction_x = "Special Instruction #{i+1}"
+            order.special_instructions << row[special_intruction_x]
           end
+          order.save
+
+          user = Spree.user_class.where(:email => row['Email'])
+          # user_group_id = Spree::UserGroup.find_by_name(row["Customer type"]).id if row["Customer type"] != "DRTC:LBT"
+          bill_country_id = Spree::Country.find_by_name(row["Billing Country"]).id
+          bill_state_id = Spree::State.find_by_name(row["Billing State"]).id
+          ship_country_id = Spree::Country.find_by_name(row["Shipping Country"]).id
+          ship_state_id = Spree::State.find_by_name(row["Shipping State"]).id
+          user_details = {"email"=> row["Email"], "bill_address_attributes"=>{"firstname"=> row["Order First Name"],
+                                                                             "lastname"=> row["Order Last Name"],
+                                                                             "company"=> row["Order Company Name"],
+                                                                             "address1"=> row["Billing Address1"],
+                                                                             "address2"=> row["Billing Address2"],
+                                                                             "city"=> row["Billing City"], "zipcode"=>row["Billing ZIP"],
+                                                                             "country_id"=> bill_country_id, "state_id"=> bill_state_id,
+                                                                             "phone"=> row["Billing Phone"]},
+                                                  "ship_address_attributes"=>{"firstname"=> row["Order First Name"],
+                                                                             "lastname"=> row["Order Last Name"],
+                                                                             "company"=> row["Order Company Name"],
+                                                                             "address1"=> row["Shipping Address1"],
+                                                                             "address2"=> row["Shipping Address2"],
+                                                                             "city"=> row["Shipping City"], "zipcode"=>row["Shipping ZIP"],
+                                                                             "country_id"=> ship_country_id, "state_id"=> ship_state_id,
+                                                                             "phone"=> row["Shipping Phone"]},
+                                                  "customer_type" => row["Customer type"] }
+
+
+          #Assing Variants
+          ::CSV.foreach(open_file2,{:headers => true}) do |row_clon|
+            if row_clon["Order Number"] == row["Order Number"]
+              #Create Shippment
+              stock_location_id = Spree::StockLocation.find_by_name(row["Fulfilled From"]).id
+
+              shipments = order.shipments.find_all_by_number(row_clon["Shipment Number"])
+              if shipments.blank?
+                shipment = order.shipments.create(:number => row_clon['Shipment Number'], :stock_location_id => stock_location_id)
+              else
+                shipment = shipments.first
+              end
+
+              variant = Spree::Variant.find_by_sku(row_clon["Sku"])
+              quantity = row_clon["Number of Items"].to_i
+              order.contents.add(variant, quantity, nil, shipment)
+
+              shipment.refresh_rates
+              shipment.save!
+
+            end
+          end
+
+
+          #Set customer details
+          order.update_attributes(user_details)
+          order.associate_user!(user.first) unless user.empty?
+
+          order.save!
+          order.refresh_shipment_rates
+
+
+          until order.payment?
+            order.next
+          end
+
+          unless row["Tax"].blank? || row["Tax"] == "0"
+            tax_adjustment = order.adjustments.new
+            tax_adjustment.label = "Custom Tax"
+            tax_adjustment.originator_type = "Spree::TaxRate"
+            tax_adjustment.amount = row["Tax"].to_f
+            tax_adjustment.save
+          end
+
+          unless row["Shipping Cost"].blank? || row["Shipping Cost"] == "0"
+            order.adjustments.shipping.first.destroy unless order.adjustments.shipping.empty?
+            shipping = order.adjustments.new
+            shipping.label = "Custom Shipping`"
+            shipping.originator_type = "Spree::ShippingMethod"
+            shipping.amount = row["Shipping Cost"].to_f
+            shipping.save
+          end
+
+          order.save
+
+          #set Payment
+          payment_method_id = Spree::PaymentMethod.find_by_name(row["Payment Method"]).id
+
+          payment_data = {"amount"=> order.total.to_f, "payment_method_id"=> payment_method_id, 
+                     "purchase_order_number"=> row["Purchase Order Number"], 
+                     "no_charge_note"=> row["No Charge Note"], 
+                     "no_charge_code"=> row["No Charge Code"]}
+          payment = order.payments.build(payment_data)
+
+          payment.save
+
+          order.from_csv = true
+          until order.completed?
+            order.next!
+          end
+
+          orders << order
         end
-
-        shipment.refresh_rates
-        shipment.save!
-
-        #Set customer details
-        order.update_attributes(user_details)
-        order.associate_user!(user.first) unless user.empty?
-
-        order.save!
-        order.refresh_shipment_rates
-
-
-        until order.payment?
-          order.next
-        end
-
-        unless row["Tax"].blank? || row["Tax"] == "0"
-          tax_adjustment = order.adjustments.new
-          tax_adjustment.label = "Custom Tax"
-          tax_adjustment.originator_type = "Spree::TaxRate"
-          tax_adjustment.amount = row["Tax"].to_f
-          tax_adjustment.save
-        end
-
-        unless row["Shipping Cost"].blank? || row["Shipping Cost"] == "0"
-          order.adjustments.shipping.first.destroy unless order.adjustments.shipping.empty?
-          shipping = order.adjustments.new
-          shipping.label = "Custom Shipping`"
-          shipping.originator_type = "Spree::ShippingMethod"
-          shipping.amount = row["Shipping Cost"].to_f
-          shipping.save
-        end
-
-        order.save
-
-        #set Payment
-        payment_method_id = Spree::PaymentMethod.find_by_name(row["Payment Method"]).id
-
-        payment_data = {"amount"=> order.total.to_f, "payment_method_id"=> payment_method_id, 
-                   "purchase_order_number"=> row["Purchase Order Number"], 
-                   "no_charge_note"=> row["No Charge Note"], 
-                   "no_charge_code"=> row["No Charge Code"]}
-        payment = order.payments.build(payment_data)
-
-        payment.save
-
-        order.from_csv = true
-        until order.completed?
-          order.next!
-        end
-
-        orders << order
+      rescue => e
+        orders = []
+        self.error!
+        message = ""
+        message << e.message
+        message << e.backtrace.join("\n")
+        errors[:error] = message
+        raise ActiveRecord::Rollback
       end
-    rescue => e
-      self.error!
-      message = ""
-      message << e.message
-      message << e.backtrace.join("\n")
-      errors = {error: message}
     end
-      self.finish! unless errors
-      ::CsvOrdersMailer.notify_admin_email(orders, self, errors).deliver
+    self.finish! unless errors
+    ::CsvOrdersMailer.notify_admin_email(orders, self, errors).deliver
   end
 
 
@@ -199,6 +213,19 @@ class Spree::CsvOrder < ActiveRecord::Base
         message["row #{$.}"] << "we couldn't find this user group: #{row['Customer type']}" if !Spree::Order.customer_types.include?(row["Customer type"]) && row["Customer type"] != "DRTC:LBT"
       end
 
+      unless row['Shipping Method'].blank?
+        ship = Spree::ShippingMethod.where(:name => row["Shipping Method"])
+        message["row #{$.}"] << "we couldn't find this shipping method: #{row['Shipping Method']}" if ship.empty?
+      end
+
+      unless row['Shipment Number'].blank?
+        message["row #{$.}"] << "shipping numbers must begins with a 'H': #{row['Shipment Number']}" unless row["Shipment Number"] =~ /^H\d+/
+      end
+
+      unless row['Fulfilled From'].blank?
+        stock = Spree::StockLocation.where(:name => row["Fulfilled From"])
+        message["row #{$.}"] << "we couldn't find this stock location: #{row['Fulfilled From"']}" if stock.empty?
+      end
 
       self.orders_number = orders_number_list.uniq.count
       unless message["row #{$.}"].empty?
