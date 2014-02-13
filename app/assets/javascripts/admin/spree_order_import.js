6b2c41370a63f406cb5b83950e5775159ad45e99
Hector Bustillos <hector.bustillos@crowdint.com>
2014-02-12 21:35:26 -0600

    protect form to be send without a file

--- app/assets/javascripts/admin/spree_order_import.js
+++ app/assets/javascripts/admin/spree_order_import.js
@@ -1,1 +1,11 @@
 //= require admin/spree_backend
+$(document).ready(
+  function(){
+    $('#csv_file').change(
+      function(){
+        if ($(this).val()) {
+          $('input:submit').attr('disabled',false); 
+        }
+    }
+  );
+});
