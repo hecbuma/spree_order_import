0b356680f8c5ddebc6595d2759e05c39d5c94a36
Hector Bustillos <hector.bustillos@crowdint.com>
2014-03-12 12:57:29 -0600

    override create_proposed_shipments correctly

--- app/models/spree/order/checkout_decorator.rb
+++ app/models/spree/order/checkout_decorator.rb
@@ -2,9 +2,11 @@ module Spree
   Order.class_eval do
     include Checkout
 
+    alias_method :original_create_proposed_shipments, :create_proposed_shipments
+
     def create_proposed_shipments
       unless from_csv
-        super
+        original_create_proposed_shipments
       end
     end
 
